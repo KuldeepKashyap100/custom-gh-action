f6e3b36a05a5e8647b7886edbff43d54df44d797
Kuldeep <kuldeepkashyap100@gmail.com>
2023-11-09 17:31:44 +0530

    added migration

---
+++ packages/db/prisma/migrations/20231109115946_test/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `org_id` on the `test_employee` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "test_employee" DROP COLUMN "org_id";
